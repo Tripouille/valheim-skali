5d40c39e788c3e4e1f76bcabd68d46de9a94d0f5
Astrid Allemandou <aallemandou@winter-mushroom.com>
2021-11-22 18:26:15 +0100

    feat: Redirect from / to /valhabba

--- next.config.js
+++ next.config.js
@@ -6,4 +6,13 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/valhabba',
+        permanent: true,
+      },
+    ];
+  },
 };
