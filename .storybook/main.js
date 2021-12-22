f85e07d85f8f7f2fe97e88421f7e629390f460bb
Tripouille <jmichel.gambard@gmail.com>
2021-12-22 19:46:38 +0100

    feat(storybook): add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,5 @@
+module.exports = {
+  stories: ['../packages/storybook/**/*.stories.tsx'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/react',
+};
