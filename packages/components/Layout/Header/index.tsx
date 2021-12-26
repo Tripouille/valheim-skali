a67a90101eada599c69990ce4fa9f0b2fa4ef60d
Chinimala <astrid.allemandou@free.fr>
2021-12-26 12:38:45 +0100

    feat(layout): add drawer for responsive menu, improve NavItem

--- packages/components/Layout/Header/index.tsx
+++ packages/components/Layout/Header/index.tsx
@@ -4,35 +4,93 @@ import { useSession } from 'next-auth/react';
 import {
   chakra,
   Center,
+  Button,
   ButtonGroup,
   IconButton,
   Menu,
   MenuButton,
   MenuList,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerBody,
+  DrawerFooter,
+  VStack,
+  useDisclosure,
+  useBreakpointValue,
 } from '@chakra-ui/react';
 import { GiVikingHelmet } from 'react-icons/gi';
 import { BiChevronDown } from 'react-icons/bi';
+import { RiMenuLine } from 'react-icons/ri';
 import { NavRoutes } from '@packages/utils/routes';
 import NavItem from '../NavItem';
 import SignInOut from './SignInOut';
 
 const Header: React.FC = () => {
   const { data: session } = useSession();
+  const menuType = useBreakpointValue({ base: 'drawer', md: 'header' });
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   // const router = useRouter();
   // const { serverName } = router.query;
   const serverName = 'valhabba';
 
   return (
-    <chakra.header height="header" bgColor="rgba(49, 130, 206, 0.7)" fontFamily="Norse">
+    <chakra.header height="header" bgColor="rgba(49, 130, 206, 0.7)">
       <Center justifyContent="space-between" h="full">
-        <chakra.nav ms="2">
-          <ButtonGroup variant="ghost">
-            <NavItem root={`/${serverName}`} navRoute={NavRoutes.HOME} />
-            <NavItem root={`/${serverName}`} navRoute={NavRoutes.RULES} />
-            <NavItem root={`/${serverName}`} navRoute={NavRoutes.EVENTS} />
-          </ButtonGroup>
-        </chakra.nav>
+        {menuType === 'header' && (
+          <chakra.nav ms="2" fontFamily="Norse">
+            <ButtonGroup variant="ghost">
+              <NavItem root={`/${serverName}`} navRoute={NavRoutes.HOME} />
+              <NavItem root={`/${serverName}`} navRoute={NavRoutes.RULES} />
+              <NavItem root={`/${serverName}`} navRoute={NavRoutes.EVENTS} />
+            </ButtonGroup>
+          </chakra.nav>
+        )}
+        {menuType === 'drawer' && (
+          <>
+            <IconButton
+              aria-label="Open menu"
+              icon={<RiMenuLine />}
+              onClick={onOpen}
+              ms="2"
+              variant="ghost"
+              fontSize="2xl"
+              title="Open menu"
+            />
+            <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+              <DrawerOverlay />
+              <DrawerContent bgColor="blue.700" fontFamily="Norse">
+                <DrawerBody>
+                  <chakra.nav ms="2" fontFamily="Norse">
+                    <VStack align="stretch">
+                      <NavItem
+                        root={`/${serverName}`}
+                        navRoute={NavRoutes.HOME}
+                        onClick={onClose}
+                      />
+                      <NavItem
+                        root={`/${serverName}`}
+                        navRoute={NavRoutes.RULES}
+                        onClick={onClose}
+                      />
+                      <NavItem
+                        root={`/${serverName}`}
+                        navRoute={NavRoutes.EVENTS}
+                        onClick={onClose}
+                      />
+                    </VStack>
+                  </chakra.nav>
+                </DrawerBody>
+                <DrawerFooter>
+                  <Button fontSize="3xl" w="full" onClick={onClose}>
+                    Fermer
+                  </Button>
+                </DrawerFooter>
+              </DrawerContent>
+            </Drawer>
+          </>
+        )}
         <Menu id="account-menu" isLazy>
           <MenuButton
             as={IconButton}
--- packages/components/Layout/NavItem/index.tsx
+++ packages/components/Layout/NavItem/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import Link from 'next/link';
+import NextLink from 'next/link';
 import { Button } from '@chakra-ui/react';
 import { NavRoutes } from '@packages/utils/routes';
 import { NavRoutesToLabel } from '@packages/utils/constants';
+import { Callback } from '@packages/utils/types';
 
 export interface NavItemProps {
   root: string;
   navRoute: NavRoutes;
+  onClick?: Callback;
 }
 
-const NavItem: React.FC<NavItemProps> = ({ root, navRoute }) => (
-  <Button fontSize="3xl">
-    <Link href={`${root}${navRoute}`}>
-      <a>{NavRoutesToLabel[navRoute]}</a>
-    </Link>
-  </Button>
+const NavItem: React.FC<NavItemProps> = ({ root, navRoute, onClick }) => (
+  <NextLink href={`${root}${navRoute}`} passHref>
+    <Button as="a" fontSize="3xl" onClick={onClick}>
+      {NavRoutesToLabel[navRoute]}
+    </Button>
+  </NextLink>
 );
 
 export default NavItem;
