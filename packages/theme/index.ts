6b037feb6c67df4235bebf014982852de25a527b
Chinimala <astrid.allemandou@free.fr>
2021-12-27 19:03:17 +0100

    feat(theme): remove scrollbar arrows

--- packages/theme/index.ts
+++ packages/theme/index.ts
@@ -13,61 +13,17 @@ const options = {
       },
       '&::-webkit-scrollbar-track': {
         backgroundColor: 'blue.900',
+        borderRadius: 'sm',
       },
       '&::-webkit-scrollbar-thumb': {
         backgroundColor: 'blue.500',
-        borderRadius: 'md',
+        borderRadius: 'sm',
       },
       '&::-webkit-scrollbar-thumb:hover': {
         backgroundColor: 'blue.300',
       },
       '&::-webkit-scrollbar-corner': {
         backgroundColor: 'blue.900',
-      },
-      '&::-webkit-scrollbar-button:single-button': {
-        backgroundColor: 'blue.900',
-        display: 'block',
-        height: '0.6rem',
-        width: '0.6rem',
-        borderStyle: 'solid',
-        borderRadius: 'sm',
-        borderColor: 'transparent',
-      },
-      /* Up */
-      '&::-webkit-scrollbar-button:single-button:vertical:decrement': {
-        borderWidth: '0 0.4rem 0.8rem 0.4rem',
-        borderBottomColor: 'blue.500',
-      },
-
-      '&::-webkit-scrollbar-button:single-button:vertical:decrement:hover': {
-        borderBottomColor: 'blue.300',
-      },
-      /* Down */
-      '&::-webkit-scrollbar-button:single-button:vertical:increment': {
-        borderWidth: '0.8rem 0.4rem 0 0.4rem',
-        borderTopColor: 'blue.500',
-      },
-
-      '&::-webkit-scrollbar-button:vertical:single-button:increment:hover': {
-        borderTopColor: 'blue.300',
-      },
-      /* Left */
-      '&::-webkit-scrollbar-button:single-button:horizontal:decrement': {
-        borderWidth: '0.4rem 0.8rem 0.4rem 0',
-        borderRightColor: 'blue.500',
-      },
-
-      '&::-webkit-scrollbar-button:single-button:horizontal:decrement:hover': {
-        borderRightColor: 'blue.300',
-      },
-      /* Right */
-      '&::-webkit-scrollbar-button:single-button:horizontal:increment': {
-        borderWidth: '0.4rem 0 0.4rem 0.8rem',
-        borderLeftColor: 'blue.500',
-      },
-
-      '&::-webkit-scrollbar-button:single-button:horizontal:increment:hover': {
-        borderLeftColor: 'blue.300',
       },
     },
   },
