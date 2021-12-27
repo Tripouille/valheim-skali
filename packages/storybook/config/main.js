ee350ae31d1867e75081dde71bde230e5395418b
Tripouille <jmichel.gambard@gmail.com>
2021-12-27 20:23:50 +0100

    feat(storybook): add jest

--- packages/storybook/config/main.js
+++ packages/storybook/config/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../packages/storybook/**/*.stories.tsx'],
+  stories: ['../../../packages/storybook/**/*.stories.tsx'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
