b1befaa7b9f9b83c1068a31e9150ddc701982fd3
Chinimala <astrid.allemandou@free.fr>
2022-01-03 19:08:44 +0100

    refactor(components): rename to Paragraphs and AccordionItem

--- packages/components/pages/Rules/GameRules/InstanceRules/index.tsx
+++ packages/components/pages/Rules/GameRules/InstanceRules/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Box, Heading } from '@chakra-ui/react';
 import { MdComputer } from 'react-icons/md';
 import { GiPig, GiVillage, GiWarPick } from 'react-icons/gi';
-import CustomText from '@packages/components/core/CustomText';
+import Paragraphs from '@packages/components/core/Paragraphs';
 import IconTitle from '@packages/components/core/IconTitle';
 import Figure from '@packages/components/core/Figure';
 import ZoomableImage from '@packages/components/core/ZoomableImage';
 
 const InstanceRules = () => (
   <>
-    <CustomText
+    <Paragraphs
       paragraphs={[
         `Ce serveur est un monde multijoueur ... et le jeu n’est pas encore complètement optimisé pour ça.`,
         `Quand vous modifiez un endroit de notre monde, en y faisant une base par exemple, tous les joueurs qui passeront par là verront et subiront cette modification. Leur ordinateur devra recevoir l’information qu’un objet a été placé, que le terrain a été modifié (creusé, surélevé, aplati) ou encore qu’un troupeau vit ici. Puis leur ordinateur devra calculer tous les impacts que cette information a sur le reste de l’environnement. Ce qui entraîne rapidement des ralentissements du jeu, pour vous comme pour les autres.`,
@@ -18,7 +18,7 @@ const InstanceRules = () => (
     />
     <Box mb="4">
       <IconTitle title="Les instances" size="sm" icon={MdComputer} iconColor="teal.100" />
-      <CustomText
+      <Paragraphs
         paragraphs={[
           `Pour mesurer le risque de ralentissements, il suffit de connaître le nombre d’éléments que votre ordinateur doit prendre en compte autour de vous. Pour cela, il suffit d’appuyer sur F2 en jeu pour ouvrir un panneau d’information et d’y lire la ligne ’instances’. Dans une prairie, il sera typiquement entre 3000 et 5000.`,
           `Au Valhabba, quelle que soit votre installation, vous devez rester en dessous de 9500 instances. Au-delà de ça, l’équipe d’administration viendra “retirer” arbitrairement vos installations pour ramener ce chiffre à 9500.`,
@@ -37,7 +37,7 @@ const InstanceRules = () => (
     </Box>
     <Box mb="4">
       <IconTitle title="Les bêtes" size="sm" icon={GiPig} iconColor="pink.300" />
-      <CustomText
+      <Paragraphs
         paragraphs={[
           `Les animaux apprivoisés entraînant également des calculs supplémentaires, leur nombre est limité. Cela nous permet aussi de réguler l’économie du serveur. Tout joueur ou regroupement de joueurs jouant ensemble, ne peut avoir, quel que soit l’emplacement de ses bêtes, qu’un maximum de 40 têtes, réparties en :`,
           `- 15 sangliers,`,
@@ -49,7 +49,7 @@ const InstanceRules = () => (
     </Box>
     <Box mb="4">
       <IconTitle title="La taille des bases" size="sm" icon={GiVillage} iconColor="orange.600" />
-      <CustomText
+      <Paragraphs
         paragraphs={[
           `Dans la même idée, avoir une base géante gênera immanquablement les autres joueurs, c’est donc interdit.`,
           `Voici les restrictions à respecter :`,
@@ -59,10 +59,10 @@ const InstanceRules = () => (
         Droits terriens des joueurs :
       </Heading>
       <strong>Joueur solo :</strong> 6 balises, trois maximum au même endroit
-      <CustomText fontSize="xs" paragraphs={[`(un endroit = un cercle de 200m de diamètre)`]} />
+      <Paragraphs fontSize="xs" paragraphs={[`(un endroit = un cercle de 200m de diamètre)`]} />
       <strong>Groupe de joueurs</strong> hors d&quot;un village officiel : pareil, mais SANS droits
       supplémentaires grâce au regroupement.
-      <CustomText
+      <Paragraphs
         fontSize="xs"
         paragraphs={[
           `(c'est à dire 6 balises partagées par tout le groupe, et 3 max au même endroit)`,
@@ -72,7 +72,7 @@ const InstanceRules = () => (
       qu&quot;il a de villageois (jusqu&quot;à 10 balises maximum). <br />
       Cependant chaque villageois n&quot;a individuellement plus le droit qu&quot;à 2 balises hors
       du village pour leur jeu personnel.
-      <CustomText
+      <Paragraphs
         mt="4"
         paragraphs={[
           `En bref, chaque joueur peut occuper son propre terrain, mais pas trop de terrain, même si vous êtes plusieurs à jouer ensemble. Le seul moyen d'avoir plus de place est de constituer un village (voir les règles spécifiques des villages au Valhabba).`,
@@ -81,14 +81,14 @@ const InstanceRules = () => (
     </Box>
     <Box mb="4">
       <IconTitle title="Le minage" size="sm" icon={GiWarPick} iconColor="silver" />
-      <CustomText
+      <Paragraphs
         paragraphs={[
           `Enfin, comme les  modifications du terrain comptent également comme des instances°, le minage sauvage contribue aussi à ralentir le jeu des autres joueurs, en plus de rendre la carte hideuse.`,
           ` Au Valhabba, il est donc interdit de miner afin de récolter le minerai qui se trouve sous vos pieds, contentez-vous de celui qu’il y a en surface. La limite n'est PAS à -15 mètres ni à -5 mètres, elle est à 0 mètre.`,
           `La seule exception étant pour l’argent. Dans ce cas, nous vous demandons de réduire la taille du minage au strict minimum.`,
         ]}
       />
-      <CustomText
+      <Paragraphs
         fontSize="xs"
         paragraphs={[
           `(° : reboucher votre trou ne fera même qu’amplifier le problème, car le jeu chargera votre trou, puis chargera son rebouchement.) `,
--- packages/components/pages/Rules/GameRules/StreamerRules/index.tsx
+++ packages/components/pages/Rules/GameRules/StreamerRules/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Box, Heading } from '@chakra-ui/react';
-import CustomText from '@packages/components/core/CustomText';
+import Paragraphs from '@packages/components/core/Paragraphs';
 
 const StreamerRules = () => (
   <>
-    <CustomText
+    <Paragraphs
       paragraphs={[
         `La seule condition que nous mettons aux streams en direct est ... LE CONSENTEMENT DE CEUX QUI SERONT ENREGISTRÉS.`,
         `Concrètement si vous souhaitez streamer, ça se passe comme ça : vous streamez quand vous voulez, et où vous voulez, dès lors que :`,
@@ -14,7 +14,7 @@ const StreamerRules = () => (
       <Heading size="s" display="flex" alignItems="center">
         1/ Tous les joueurs qui apparaissent à votre écran vous ont donné leur accord.
       </Heading>
-      <CustomText
+      <Paragraphs
         paragraphs={[
           `Si vous tombez sur quelqu'un de manière inattendue vous devez lui mettre dans le chat du jeu un message du style “Je streame en direct mon jeu, est ce que ça te dérange d'y apparaître ?”. À vous de trouver votre propre formule, mais elle doit dire clairement et sans ambigüité que vous enregistrez.`,
           `Si la personne vous dit oui, nickel, amusez-vous 🤗 Si elle vous dit non, c’est à vous de vous éloigner du joueur afin qu'il n'apparaisse plus dans votre stream.`,
@@ -26,7 +26,7 @@ const StreamerRules = () => (
         2/ Si vous êtes souhaitez streamer vos échanges vocaux dans notre discord, vous devez
         impérativement être dans le canal vocal &quot;Stream en direct&quot;
       </Heading>
-      <CustomText
+      <Paragraphs
         paragraphs={[
           `Lorsqu’une personne vous y rejoint, vous devez la prévenir que vous enregistrez. En revanche, si ça ne plaît pas à celle-ci, c'est à elle de quitter le salon vocal.`,
           `De cette manière, ceux qui vous rejoignent seront doublement prévenus que leur voix est enregistrée (le nom du salon + votre annonce à chaque arrivée).`,
@@ -40,7 +40,7 @@ const StreamerRules = () => (
           [stream : 🟢]
         </Box>
       </Heading>
-      <CustomText
+      <Paragraphs
         paragraphs={[
           `Afin d'être encore plus clair vis-à-vis des autres,  changez votre pseudo Discord et accolez-lui le préfix :  [stream : 🟢 ] (clic droit, changez le pseudo etc).`,
           `Exemple : si l’admin Gyda Selordotyr voulait streamer, il passerait du pseudo, “Gyda Selordotyr”, à  “[stream : 🟢] Gyda Selordotyr”.`,
