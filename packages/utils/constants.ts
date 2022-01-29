ffb06c925bbed1e789ade3522ded386675d3d5db
Chinimala <astrid.allemandou@free.fr>
2022-01-29 22:01:15 +0100

    fix(tag): fix predefined colors to hexadecimal

--- packages/utils/constants.ts
+++ packages/utils/constants.ts
@@ -15,8 +15,9 @@ export enum StoryCategory {
   PAGE = 'page',
 }
 
+/** Predefined colors for tags (must be hexadecimal for text color computation) */
 export const TagColors: Record<string, string> = {
-  PvP: 'darkred',
-  PvE: '#0047AB',
-  Continu: 'blue.400',
+  PvP: '#8b0000', // darkred
+  PvE: '#0047AB', // cobalt
+  Continu: '#4299e1', // blue.400
 };
