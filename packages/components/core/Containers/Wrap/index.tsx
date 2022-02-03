0e3b32caff464f8813a05869c9cf5c8bed6fed62
Chinimala <astrid.allemandou@free.fr>
2022-02-03 20:15:02 +0100

    feat: add Wrap component, use for event tags

---
+++ packages/components/core/Containers/Wrap/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Wrap as ChakraWrap,
+  WrapProps as ChakraWrapProps,
+  WrapItem as ChakraWrapItem,
+  WrapItemProps as ChakraWrapItemProps,
+} from '@chakra-ui/react';
+
+export type WrapProps = ChakraWrapProps;
+
+export const Wrap: React.FC<WrapProps> = chakraWrapProps => (
+  <ChakraWrap {...chakraWrapProps}></ChakraWrap>
+);
+
+export type WrapItemProps = ChakraWrapItemProps;
+
+export const WrapItem: React.FC<WrapItemProps> = chakraWrapItemProps => (
+  <ChakraWrapItem {...chakraWrapItemProps}></ChakraWrapItem>
+);
