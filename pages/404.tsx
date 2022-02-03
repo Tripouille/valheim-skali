9c481b9ef13901e4e0721b71778b7ff3a077d2cd
Chinimala <astrid.allemandou@free.fr>
2022-02-03 18:19:33 +0100

    feat(not-found): add NotFound component, use for 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import { chakra } from '@chakra-ui/react';
-import Card from '@packages/components/core/Containers/Card';
+import NotFound from '@packages/components/core/Feedback/NotFound';
 
-const NotFoundPage: React.FC = () => (
-  <Card w="xl" h="3xs">
-    <chakra.h1 fontSize="xl">
-      404 - La page que vous recherchez n&apos;existe pas ¯\_(ツ)_/¯
-    </chakra.h1>
-  </Card>
-);
+const NotFoundPage: React.FC = () => <NotFound />;
 
 export default NotFoundPage;
