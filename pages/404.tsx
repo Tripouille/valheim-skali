230e73ce70da33c191a47155952fde78f6bd54eb
Chinimala <astrid.allemandou@free.fr>
2022-02-03 19:51:11 +0100

    feat: replace NotFound with generic Error, use in client error page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import NotFound from '@packages/components/core/Feedback/NotFound';
+import Error from '@packages/components/core/Feedback/Error';
 
-const NotFoundPage: React.FC = () => <NotFound />;
+const NotFoundPage: React.FC = () => (
+  <Error>404 - La page que vous recherchez n&apos;existe pas ¯\_(ツ)_/¯</Error>
+);
 
 export default NotFoundPage;
