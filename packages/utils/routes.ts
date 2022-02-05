053bda8899f0464a78fa8a2446e197838275e9ee
Chinimala <astrid.allemandou@free.fr>
2022-02-05 14:29:08 +0100

    feat: add pages for mods, world and price list

--- packages/utils/routes.ts
+++ packages/utils/routes.ts
@@ -6,6 +6,9 @@ enum NavRoutes {
   HOME = '/',
   RULES = '/rules',
   EVENTS = '/events',
+  PRICE_LIST = '/price-list',
+  MODS = '/mods',
+  WORLD = '/world',
 }
 
 export { APIRoutes, NavRoutes };
