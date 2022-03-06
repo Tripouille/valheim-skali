9b53f8f0c928812265f14d40bb02ac55d7f7a005
Chinimala <astrid.allemandou@free.fr>
2022-03-06 13:44:42 +0100

    feat(secured): add fallback component to Secured

--- packages/components/core/Authentication/Secured.tsx
+++ packages/components/core/Authentication/Secured.tsx
@@ -1,22 +1,18 @@
 import { useSession } from '@packages/utils/hooks/useSession';
 import { Children } from '@packages/utils/types';
-import { Permissions, SessionStatus, userHasRequiredPermissions } from '@packages/utils/auth';
+import { Permissions } from '@packages/utils/auth';
 
 export interface SecuredProps {
   permissions: Permissions;
   children: Children;
+  fallback?: Children;
 }
 
-const Secured: React.FC<SecuredProps> = ({ permissions, children }) => {
+const Secured: React.FC<SecuredProps> = ({ permissions, children, fallback }) => {
   const session = useSession();
 
-  if (
-    session.status === SessionStatus.AUTHENTICATED &&
-    userHasRequiredPermissions(session.data.permissions, permissions)
-  ) {
-    return <>{children}</>;
-  }
-
+  if (session.hasRequiredPermissions(permissions)) return <>{children}</>;
+  if (fallback) return <>{fallback}</>;
   return null;
 };
 
