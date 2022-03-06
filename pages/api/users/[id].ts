476711e9a8578b219fc497f0518661092f409e1e
Chinimala <astrid.allemandou@free.fr>
2022-03-06 16:49:19 +0100

    feat(admin): restrict user nameInGame length

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -2,11 +2,31 @@ import { NextApiHandler, NextApiRequest as Req, NextApiResponse as Res } from 'n
 import { ObjectId } from 'bson';
 import { PermissionCategory, PermissionPrivilege } from '@packages/utils/auth';
 import { requirePermissions } from '@packages/utils/api/auth';
-import { isUpdatableUserData, User, UserInDb } from '@packages/data/user';
+import {
+  isUpdatableUserData,
+  UpdatedUserPartialData,
+  User,
+  UserInDb,
+  UserWithInfosInDb,
+  USER_NAME_IN_GAME_MAX_LENGTH,
+} from '@packages/data/user';
 import db from '@packages/utils/api/db';
 import { ServerException, updateOneInCollection } from '@packages/utils/api/api';
 
 const collectionName = 'users';
+
+const generateGetNewFieldValue =
+  (userNewData: UpdatedUserPartialData, oldUser: UserInDb) =>
+  <K extends keyof Required<UpdatedUserPartialData>>(
+    key: K,
+    transform: (newValue: Required<UpdatedUserPartialData>[K]) => UserWithInfosInDb[K],
+    fallback: UserWithInfosInDb[K],
+  ): UserWithInfosInDb[K] =>
+    key in userNewData
+      ? transform(userNewData[key] as Required<UpdatedUserPartialData>[K])
+      : key in oldUser
+      ? (oldUser[key as keyof UserInDb] as UserWithInfosInDb[K])
+      : fallback;
 
 const patchUser = async (req: Req, res: Res) => {
   const { id } = req.query as { id: string };
@@ -17,16 +37,21 @@ const patchUser = async (req: Req, res: Res) => {
   const oldUser = await db.findOne<UserInDb>(collectionName, { _id: new ObjectId(id) });
   if (!oldUser) throw new ServerException(404);
 
-  const newRoleIds =
-    'roleIds' in userNewData
-      ? userNewData.roleIds.map(roleId => new ObjectId(roleId))
-      : 'roleIds' in oldUser
-      ? oldUser.roleIds
-      : [];
+  const getNewFieldValue = generateGetNewFieldValue(userNewData, oldUser);
+  const newRoleIds = getNewFieldValue(
+    'roleIds',
+    roleIds => roleIds.map(roleId => new ObjectId(roleId)),
+    [],
+  );
+  const newNameInGame = getNewFieldValue(
+    'nameInGame',
+    nameInGame => nameInGame.substring(0, USER_NAME_IN_GAME_MAX_LENGTH),
+    undefined,
+  );
   const result = await updateOneInCollection<UserInDb>(collectionName, id, {
     ...oldUser,
-    ...userNewData,
     roleIds: newRoleIds,
+    nameInGame: newNameInGame,
   });
   if (!result.ok) throw new ServerException(500);
 
