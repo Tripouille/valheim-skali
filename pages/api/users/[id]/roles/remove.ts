c62c37a38555f571c173d4d5495a02e95126617f
Chinimala <astrid.allemandou@free.fr>
2022-03-07 11:30:03 +0100

    fix(admin): role read permission is required to add/remove role

--- pages/api/users/[id]/roles/remove.ts
+++ pages/api/users/[id]/roles/remove.ts
@@ -1,13 +1,10 @@
 import { NextApiHandler, NextApiRequest as Req, NextApiResponse as Res } from 'next';
-import { PermissionCategory, PermissionPrivilege } from '@packages/utils/auth';
-import { requirePermissions } from '@packages/api/auth';
 import { ServerException } from '@packages/api/common';
 import { Action, addOrRemoveRoleToUser } from '@packages/api/users/addOrRemoveRoleToUser';
 
 const userRolesHandler: NextApiHandler = async (req: Req, res: Res) => {
   try {
     if (req.method === 'PATCH') {
-      await requirePermissions({ [PermissionCategory.USER]: PermissionPrivilege.READ_WRITE }, req);
       await addOrRemoveRoleToUser(Action.REMOVE, req, res);
     }
   } catch (e) {
