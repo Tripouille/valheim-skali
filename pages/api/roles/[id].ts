dc15668184b8407817191464184a5cca856c073f
Chinimala <astrid.allemandou@free.fr>
2022-03-09 13:16:27 +0100

    feat(admin): delete role

--- pages/api/roles/[id].ts
+++ pages/api/roles/[id].ts
@@ -1,11 +1,14 @@
 import { NextApiHandler } from 'next';
 import { ServerException } from '@packages/api/common';
 import putRole from '@packages/api/roles/putRole';
+import deleteRole from '@packages/api/roles/deleteRole';
 
 const rolesHandler: NextApiHandler = async (req, res) => {
   try {
     if (req.method === 'PUT') {
       await putRole(req, res);
+    } else if (req.method === 'DELETE') {
+      await deleteRole(req, res);
     } else {
       throw new ServerException(501);
     }
