c65312ab8b46f7916a1716bac9a6da1afeabe3a4
Chinimala <astrid.allemandou@free.fr>
2022-03-09 16:00:49 +0100

    feat(admin): create role (backend)

--- packages/api/roles/putRole.ts
+++ packages/api/roles/putRole.ts
@@ -7,34 +7,15 @@ import {
   UpdateRoleData,
 } from '@packages/data/role';
 import {
-  isAdminPrivilege,
   isAdminRole,
   isSpecialRole,
   PermissionCategory,
   PermissionPrivilege,
-  Permissions,
 } from '@packages/utils/auth';
 import { requirePermissions } from '@packages/api/auth';
 import { ServerException, updateOneInCollection } from '@packages/api/common';
 import db from '../db';
-
-const isPermissions = (value: unknown): value is Permissions => {
-  return (
-    typeof value === 'object' &&
-    !!value &&
-    Object.entries(value).every(
-      ([category, privilege]) =>
-        Object.values(PermissionCategory).includes(category as PermissionCategory) &&
-        Object.values(PermissionPrivilege).includes(privilege),
-    )
-  );
-};
-
-const roleKeyToValueTypeCheck: Record<keyof UpdateRoleData, (value: unknown) => boolean> = {
-  name: value => typeof value === 'string',
-  permissions: value => isPermissions(value),
-  requiredPermissionsToAssign: value => isPermissions(value),
-};
+import { checkRoleData, deleteNonePrivileges, roleKeyToValueTypeCheck } from './utils';
 
 const isUpdateRoleDataProperty = ([key, value]: [key: string, value: unknown]) => {
   return (
@@ -57,15 +38,6 @@ const checkPermissionsIfRoleIsSpecial = async (role: RoleInDb, roleNewData: Upda
   }
 };
 
-const deleteNonePrivileges = (permissions: Permissions) => {
-  for (const [category, privilege] of Object.entries(permissions) as [
-    PermissionCategory,
-    PermissionPrivilege,
-  ][]) {
-    if (privilege === PermissionPrivilege.NONE) delete permissions[category];
-  }
-};
-
 const getRoleNewDataForDb = (roleNewData: UpdateRoleData, role: RoleInDb): RoleInDb => {
   roleNewData.name = roleNewData.name?.substring(0, ROLE_NAME_IN_GAME_MAX_LENGTH);
   roleNewData.permissions = { ...role.permissions, ...roleNewData.permissions };
@@ -73,43 +45,9 @@ const getRoleNewDataForDb = (roleNewData: UpdateRoleData, role: RoleInDb): RoleI
     ...role.requiredPermissionsToAssign,
     ...roleNewData.requiredPermissionsToAssign,
   };
-  deleteNonePrivileges(role.permissions);
-  deleteNonePrivileges(role.requiredPermissionsToAssign);
+  deleteNonePrivileges(roleNewData.permissions);
+  deleteNonePrivileges(roleNewData.requiredPermissionsToAssign);
   return roleNewData as RoleInDb;
-};
-
-const checkRoleData = (newRole: RoleInDb) => {
-  /** It is forbidden to give admin privileges */
-  for (const privilege of Object.values(newRole.permissions)) {
-    if (isAdminPrivilege(privilege)) throw new ServerException(403);
-  }
-  /** It is forbidden to have USER READ_WRITE but not ROLE READ on a role,
-   * because editing a user means assigning roles to it
-   */
-  if (
-    (newRole.permissions[PermissionCategory.USER] ?? PermissionPrivilege.NONE) >=
-      PermissionPrivilege.READ_WRITE &&
-    (newRole.permissions[PermissionCategory.ROLE] ?? PermissionPrivilege.NONE) <
-      PermissionPrivilege.READ
-  ) {
-    throw new ServerException(403);
-  }
-  /** A role must require at least user write permission to be assigned */
-  if (
-    (newRole.requiredPermissionsToAssign[PermissionCategory.USER] ?? PermissionPrivilege.NONE) <
-    PermissionPrivilege.READ_WRITE
-  ) {
-    throw new ServerException(403);
-  }
-  /** A role with user write permission must require admin privilege to be assigned */
-  if (
-    (newRole.permissions[PermissionCategory.USER] ?? PermissionPrivilege.NONE) >=
-      PermissionPrivilege.READ_WRITE &&
-    (newRole.requiredPermissionsToAssign[PermissionCategory.USER] ?? PermissionPrivilege.NONE) <
-      PermissionPrivilege.ADMIN
-  ) {
-    throw new ServerException(403);
-  }
 };
 
 const putRole = async (req: Req, res: Res) => {
@@ -127,6 +65,13 @@ const putRole = async (req: Req, res: Res) => {
 
   const newRole = getRoleNewDataForDb(roleNewData, role);
   checkRoleData(newRole);
+
+  /** Name cannot already exist */
+  const sameNameRole = await db.findOne<RoleInDb>(rolesCollectionName, {
+    name: newRole.name,
+    _id: { $ne: new ObjectId(id) },
+  });
+  if (sameNameRole) throw new ServerException(409);
 
   const result = await updateOneInCollection<RoleInDb>(rolesCollectionName, id, newRole);
   if (!result.ok) throw new ServerException(500);
