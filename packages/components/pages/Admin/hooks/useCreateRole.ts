1f781411b0389123eaf3d4cc084af996cee140f0
Chinimala <astrid.allemandou@free.fr>
2022-03-09 16:20:11 +0100

    feat(admin): check role name frontend (+ some fixs)

--- packages/components/pages/Admin/hooks/useCreateRole.ts
+++ packages/components/pages/Admin/hooks/useCreateRole.ts
@@ -1,9 +1,11 @@
-import { UseMutationOptions } from 'react-query';
+import { useCallback } from 'react';
+import { UseMutationOptions, useQueryClient } from 'react-query';
 import axios from 'axios';
 import { CreateRoleData } from '@packages/data/role';
 import { APIRoute } from '@packages/utils/routes';
-import { QueryKeys } from '@packages/utils/queryClient';
+import { QueryKeys, QueryTypes } from '@packages/utils/queryClient';
 import useOptimisticMutation from '@packages/utils/hooks/useOptimisticMutation';
+import { displayErrorToast } from '@packages/utils/toast';
 
 const createRoleOnServer = async (roleData: CreateRoleData) => {
   await axios.post(`${APIRoute.ROLES}`, roleData);
@@ -12,12 +14,26 @@ const createRoleOnServer = async (roleData: CreateRoleData) => {
 const useCreateRole = (
   onSuccess: UseMutationOptions<void, unknown, CreateRoleData>['onSuccess'],
 ) => {
-  const createRole = useOptimisticMutation<QueryKeys.ROLES, CreateRoleData>(
+  const queryClient = useQueryClient();
+
+  const createRoleMutate = useOptimisticMutation<QueryKeys.ROLES, CreateRoleData>(
     QueryKeys.ROLES,
     createRoleOnServer,
     (previousRoles, roleData) => [...(previousRoles ?? []), { ...roleData, _id: 'new' }],
     'Le rôle a bien été créé.',
     { onSuccess },
+  );
+
+  const createRole = useCallback(
+    (roleData: CreateRoleData) => {
+      const roles = queryClient.getQueryData<QueryTypes[QueryKeys.ROLES]>(QueryKeys.ROLES);
+      if (roles && roles.some(role => role.name === roleData.name)) {
+        displayErrorToast({ title: 'Erreur', description: 'Ce nom de rôle existe déjà.' });
+        return;
+      }
+      createRoleMutate(roleData);
+    },
+    [createRoleMutate, queryClient],
   );
 
   return createRole;
--- packages/components/pages/Admin/hooks/useUpdateRole.ts
+++ packages/components/pages/Admin/hooks/useUpdateRole.ts
@@ -1,9 +1,11 @@
+import { useCallback } from 'react';
 import { useQueryClient } from 'react-query';
 import axios from 'axios';
 import { Role, UpdateRoleData } from '@packages/data/role';
 import { APIRoute } from '@packages/utils/routes';
-import { QueryKeys } from '@packages/utils/queryClient';
+import { QueryKeys, QueryTypes } from '@packages/utils/queryClient';
 import useOptimisticMutation from '@packages/utils/hooks/useOptimisticMutation';
+import { displayErrorToast } from '@packages/utils/toast';
 
 const updateRoleOnServer = (updatedRole: Role) => async (updateRoleData: UpdateRoleData) => {
   await axios.put(`${APIRoute.ROLES}/${updatedRole._id}`, updateRoleData);
@@ -12,7 +14,7 @@ const updateRoleOnServer = (updatedRole: Role) => async (updateRoleData: UpdateR
 const useUpdateRole = (updatedRole: Role) => {
   const queryClient = useQueryClient();
 
-  const updateRole = useOptimisticMutation<QueryKeys.ROLES, UpdateRoleData>(
+  const updateRoleMutate = useOptimisticMutation<QueryKeys.ROLES, UpdateRoleData>(
     QueryKeys.ROLES,
     updateRoleOnServer(updatedRole),
     (previousRoles, updateRoleData) =>
@@ -21,6 +23,21 @@ const useUpdateRole = (updatedRole: Role) => {
       ) ?? [],
     'Le rôle a bien été mis à jour.',
     { onSettled: () => queryClient.invalidateQueries(QueryKeys.SESSION) },
+  );
+
+  const updateRole = useCallback(
+    (roleData: UpdateRoleData) => {
+      const roles = queryClient.getQueryData<QueryTypes[QueryKeys.ROLES]>(QueryKeys.ROLES);
+      if (
+        roles &&
+        roles.some(role => role.name === roleData.name && role._id !== updatedRole._id)
+      ) {
+        displayErrorToast({ title: 'Erreur', description: 'Ce nom de rôle existe déjà.' });
+        return;
+      }
+      updateRoleMutate(roleData);
+    },
+    [updateRoleMutate, queryClient, updatedRole],
   );
 
   return updateRole;
