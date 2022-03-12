3a6d07f2ab4f670cbba0fde8dcfc5255a5b3ad2c
Chinimala <astrid.allemandou@free.fr>
2022-03-12 21:54:31 +0100

    refactor(auth): remove SecuredPage, no page needs signin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,15 @@
-import { NextComponentType } from 'next';
-import type { AppInitialProps } from 'next/app';
+import { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { ChakraProvider } from '@chakra-ui/react';
-import { AuthConfig } from '@packages/utils/auth';
 import Layout from '@packages/components/Layout';
 import Fonts from '@packages/components/Layout/Fonts';
 import theme from '@packages/theme';
-import SecuredPage from '@packages/components/core/Authentication/SecuredPage';
 import { queryClient } from '@packages/utils/queryClient';
 
-type MyAppProps = AppInitialProps & {
-  Component: NextComponentType & AuthConfig;
-};
-
-const MyApp = ({ Component, pageProps }: MyAppProps) => (
+const MyApp = ({ Component, pageProps }: AppProps) => (
   <ChakraProvider theme={theme}>
     <QueryClientProvider client={queryClient}>
       <Head>
@@ -26,13 +19,7 @@ const MyApp = ({ Component, pageProps }: MyAppProps) => (
       </Head>
       <Fonts />
       <Layout>
-        {Component.needAuth ? (
-          <SecuredPage permissions={Component.needAuth.permissions}>
-            <Component {...pageProps} />
-          </SecuredPage>
-        ) : (
-          <Component {...pageProps} />
-        )}
+        <Component {...pageProps} />
       </Layout>
       <ReactQueryDevtools />
     </QueryClientProvider>
--- pages/valhabba/admin/[route].tsx
+++ pages/valhabba/admin/[route].tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { MenuRoute } from '@packages/utils/routes';
-import { ComponentWithAuth, ROUTES_TO_PERMISSIONS } from '@packages/utils/auth';
 import Admin from '@packages/components/pages/Admin';
 
-const AdminPage: ComponentWithAuth = () => <Admin />;
+const AdminPage = () => <Admin />;
 
-AdminPage.needAuth = { permissions: ROUTES_TO_PERMISSIONS[MenuRoute.ADMIN] };
 export default AdminPage;
