8b2d3b9dec7dc28b9d5fb70abf6c85bf0ba9d831
Chinimala <astrid.allemandou@free.fr>
2022-03-17 19:45:00 +0100

    feat(toast): add Toast component, center toast close button

--- packages/utils/toast.tsx
+++ packages/utils/toast.tsx
@@ -1,30 +1,42 @@
 import { createStandaloneToast, UseToastOptions } from '@chakra-ui/react';
+import Toast from '@packages/components/core/Feedback/Toast';
 import theme from '@packages/theme';
 
 const toast = createStandaloneToast({ theme });
 
 const commonToastOptions: UseToastOptions = {
   duration: 3000,
-  isClosable: true,
   position: 'bottom-right',
 };
 
 export const displayErrorToast = (options: UseToastOptions) => {
   toast({
     ...commonToastOptions,
-    status: 'error',
-    variant: 'subtle',
     containerStyle: { color: 'darkred' },
-    ...options,
+    render: ({ id, onClose }) => (
+      <Toast
+        id={id}
+        onClose={onClose}
+        status="error"
+        title={options.title}
+        description={options.description}
+      />
+    ),
   });
 };
 
 export const displaySuccessToast = (options: UseToastOptions) => {
   toast({
     ...commonToastOptions,
-    status: 'success',
-    variant: 'solid',
     containerStyle: { color: 'darkgreen' },
-    ...options,
+    render: ({ id, onClose }) => (
+      <Toast
+        id={id}
+        onClose={onClose}
+        status="success"
+        title={options.title}
+        description={options.description}
+      />
+    ),
   });
 };
