f3558b2c0be436b570452586df44c5c8df980dda
Chinimala <astrid.allemandou@free.fr>
2022-03-31 17:20:24 +0200

    refactor(users): adapt and use FormModal for user form

--- packages/components/core/Form/FormModal.tsx
+++ packages/components/core/Form/FormModal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Callback, Children } from '@packages/utils/types';
 import { getDataValue, DataAttributes } from '@packages/utils/dataAttributes';
 import {
@@ -16,19 +16,21 @@ import DeletePopover from './DeletePopover';
 
 /** T = CreateObjectData */
 export type FormModalProps<T> = DataAttributes & {
+  /** Set initial focus to close button or first tabbable element in modal */
+  initialFocusOnCloseButton?: boolean;
   /** Modal is open */
   isOpen: boolean;
   /** Function to close the modal */
   onClose: Callback;
-  formData: Partial<T>;
+  formData?: Partial<T>;
   /** Function returning validation error message or null */
-  getValidationError: (object: Partial<T>) => string | null;
+  getValidationError?: (object: Partial<T>) => string | null;
   canSubmit?: boolean;
-  onSubmit: (newObject: T) => void;
+  onSubmit?: (newObject: T) => void;
   /** Form */
   children: Children;
 } & (
-    | { isEdition: false; onDelete?: never }
+    | { isEdition: false }
     | {
         isEdition: true;
         canDelete?: boolean;
@@ -44,47 +46,58 @@ const FormModal = <T extends object>(props: FormModalProps<T>) => {
   const [validationError, setValidationError] = useState<string | null>(null);
 
   useEffect(() => {
-    setValidationError(getValidationError(formData));
+    if (getValidationError && formData) {
+      setValidationError(getValidationError(formData));
+    }
   }, [formData, getValidationError]);
 
   const handleSubmit = () => {
     /** If validationError is null, formData is T */
-    if (validationError === null) onSubmit(formData as T);
+    if (validationError === null && onSubmit) onSubmit(formData as T);
   };
 
+  const canDelete = props.isEdition && (props.canDelete ?? true);
+
+  const testRef = useRef(null);
+
   return (
-    <Modal isOpen={props.isOpen} onClose={props.onClose}>
+    <Modal isOpen={props.isOpen} onClose={props.onClose} initialFocusRef={testRef}>
       <ModalOverlay />
       <ModalContent>
         {children}
-        <ModalFooter>
-          <Center w="full">
-            <ButtonGroup>
-              {props.isEdition && (props.canDelete ?? true) && (
-                <Box>
-                  <DeletePopover
-                    dataCy={dataCy}
-                    onDelete={props.onDelete}
-                    deleteLabel={props.deleteLabel}
-                    deletePopoverBody={props.deletePopoverBody}
-                  />
-                </Box>
-              )}
-              {canSubmit && (
-                <Button
-                  dataCy={getDataValue(dataCy, 'submit')}
-                  colorScheme="green"
-                  onClick={handleSubmit}
-                  isDisabled={!!validationError}
-                  title={validationError ?? undefined}
-                >
-                  {props.isEdition ? 'Valider les changements' : 'Valider la création'}
-                </Button>
-              )}
-            </ButtonGroup>
-          </Center>
-        </ModalFooter>
-        <ModalCloseButton dataCy={getDataValue(dataCy, 'close_button')} />
+        {(canSubmit || canDelete) && (
+          <ModalFooter>
+            <Center w="full">
+              <ButtonGroup>
+                {canDelete && (
+                  <Box>
+                    <DeletePopover
+                      dataCy={dataCy}
+                      onDelete={props.onDelete}
+                      deleteLabel={props.deleteLabel}
+                      deletePopoverBody={props.deletePopoverBody}
+                    />
+                  </Box>
+                )}
+                {canSubmit && (
+                  <Button
+                    dataCy={getDataValue(dataCy, 'submit')}
+                    colorScheme="green"
+                    onClick={handleSubmit}
+                    isDisabled={!!validationError}
+                    title={validationError ?? undefined}
+                  >
+                    {props.isEdition ? 'Valider les changements' : 'Valider la création'}
+                  </Button>
+                )}
+              </ButtonGroup>
+            </Center>
+          </ModalFooter>
+        )}
+        <ModalCloseButton
+          dataCy={getDataValue(dataCy, 'close_button')}
+          tabIndex={props.initialFocusOnCloseButton ? 1 : 0}
+        />
       </ModalContent>
     </Modal>
   );
