efdd2d7f9f89c845634d3475dece3fea1ece5699
Chinimala <astrid.allemandou@free.fr>
2022-05-01 14:33:27 +0200

    fix(users): don't display users while loading filter and roles

--- components/pages/Admin/hooks/useUsers.ts
+++ components/pages/Admin/hooks/useUsers.ts
@@ -1,5 +1,4 @@
-import { useCallback } from 'react';
-import { useQuery } from 'react-query';
+import { useQuery, UseQueryResult } from 'react-query';
 import axios from 'axios';
 import { User } from 'data/user';
 import { APIRoute } from 'utils/routes';
@@ -19,20 +18,17 @@ export const useUsers = (filter: UserQueryFilter | false) => {
   const session = useSession();
   const { data: roles } = useRoles();
 
-  const filterUsers = useCallback(
-    (allUsers: QueryTypes[QueryKeys.USERS]) => {
-      if (!roles || !filter) return allUsers;
-      const isMemberRequiredResult = filter === UserQueryFilter.MEMBER ? true : false;
-      return allUsers.filter(user => {
-        const userRoles = getUserRoles(user, roles);
-        return (
-          userRoles.some(role => role && role.name === SpecialRoleName.MEMBER) ===
-          isMemberRequiredResult
-        );
-      });
-    },
-    [filter, roles],
-  );
+  const filterUsers = (allUsers: QueryTypes[QueryKeys.USERS]) => {
+    if (!roles || !filter) return allUsers;
+    const isMemberRequiredResult = filter === UserQueryFilter.MEMBER ? true : false;
+    return allUsers.filter(user => {
+      const userRoles = getUserRoles(user, roles);
+      return (
+        userRoles.some(role => role && role.name === SpecialRoleName.MEMBER) ===
+        isMemberRequiredResult
+      );
+    });
+  };
 
   const usersQuery = useQuery(QueryKeys.USERS, getUsers, {
     enabled: session.hasRequiredPermissions({
@@ -41,5 +37,8 @@ export const useUsers = (filter: UserQueryFilter | false) => {
     select: filterUsers,
   });
 
-  return usersQuery;
+  let status = usersQuery.status;
+  if (usersQuery.status === 'success' && (!roles || !filter)) status = 'loading';
+
+  return { ...usersQuery, status } as UseQueryResult<User[]>;
 };
