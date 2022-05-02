c9fd4faf65132bdcb9c80adf96f78ecc34212442
Chinimala <astrid.allemandou@free.fr>
2022-05-02 09:24:53 +0200

    feat(events): speed up events rendering with SSG instead of SSR

--- pages/valhabba/events/index.tsx
+++ pages/valhabba/events/index.tsx
@@ -1,17 +1,28 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import React from 'react';
-import getEvents from 'api-utils/events/getEvents';
+import { EventInDb, eventsCollectionName } from 'data/event';
+import db from 'api-utils/db';
+import { getVisitorPermissions } from 'api-utils/auth';
 import getHydrationProps from 'utils/hydration';
 import { QueryKeys } from 'utils/queryClient';
+import { permissionsMeetRequirement, PermissionCategory, PermissionPrivilege } from 'utils/auth';
 import Events from 'components/pages/Events';
 
 const EventsPage = () => <Events />;
 
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+// TODO: refactor this logic once applied to another page
+export const getStaticProps: GetStaticProps = async () => {
   return {
     props: await getHydrationProps(async queryClient => {
-      const events = await getEvents(req);
-      queryClient.setQueryData(QueryKeys.EVENTS, events);
+      const visitorPermissions = await getVisitorPermissions();
+      const requiredPermissionsToReadEvents = {
+        [PermissionCategory.EVENT]: PermissionPrivilege.READ,
+      };
+
+      if (permissionsMeetRequirement(visitorPermissions, requiredPermissionsToReadEvents)) {
+        const events = await db.find<EventInDb>(eventsCollectionName);
+        queryClient.setQueryData(QueryKeys.EVENTS, events);
+      }
     }),
   };
 };
