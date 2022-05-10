74b53c2e455cc3dc98a2a17bf01e22a619e153cb
Chinimala <astrid.allemandou@free.fr>
2022-05-10 19:59:09 +0200

    test(storybook): set chromatic delay for Home story

--- storybook/pages/Home.stories.tsx
+++ storybook/pages/Home.stories.tsx
@@ -2,7 +2,9 @@ import { storybookSetup } from 'storybook/config/setup';
 import { StoryCategory } from 'storybook/config/constants';
 import HomePage from 'components/pages/Home';
 
-const { defaultExport, StoryFactory } = storybookSetup(HomePage, StoryCategory.PAGE);
+const { defaultExport, StoryFactory } = storybookSetup(HomePage, StoryCategory.PAGE, {
+  parameters: { chromatic: { delay: 1000 } },
+});
 
 export default defaultExport;
 
