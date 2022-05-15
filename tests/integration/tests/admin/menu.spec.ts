1d8b314a17930bc539d3812d97169253def9246b
Chinimala <astrid.allemandou@free.fr>
2022-05-15 18:42:31 +0200

    test: fix cypress tests

--- tests/integration/tests/admin/menu.spec.ts
+++ tests/integration/tests/admin/menu.spec.ts
@@ -7,16 +7,16 @@ describe('menu', () => {
     cy.login();
   });
 
-  it('should only see users navigation buttons', () => {
+  it('should see users navigation buttons', () => {
     cy.setPermission(SpecialRoleName.MEMBER, PermissionCategory.USER, PermissionPrivilege.READ);
     cy.visit(`/${serverName}${MenuRoute.ADMIN}`);
     cy.dataCy('admin-nav').dataCy(ROUTES_TO_LABEL['/members']).should('be.visible');
     cy.dataCy('admin-nav').dataCy(ROUTES_TO_LABEL['/non-members']).should('be.visible');
-    cy.dataCy('admin-nav').dataCy(ROUTES_TO_LABEL['/roles']).should('not.exist');
   });
 
-  it('should only see role navigation button', () => {
-    cy.setPermission(SpecialRoleName.MEMBER, PermissionCategory.USER, PermissionPrivilege.READ);
+  it('should see role navigation button', () => {
+    cy.setPermission(SpecialRoleName.MEMBER, PermissionCategory.ROLE, PermissionPrivilege.READ);
     cy.visit(`/${serverName}${MenuRoute.ADMIN}`);
+    cy.dataCy('admin-nav').dataCy(ROUTES_TO_LABEL['/roles']).should('be.visible');
   });
 });
