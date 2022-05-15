37a14e1b196c467a3b5d6afaabeacaf25a101951
Chinimala <astrid.allemandou@free.fr>
2022-05-15 14:12:34 +0200

    test: add cypress tests for roles

---
+++ tests/integration/tests/admin/roles.spec.ts
@@ -0,0 +1,170 @@
+import { PermissionCategory, PermissionPrivilege, SpecialRoleName } from 'utils/auth';
+import { APIRoute } from 'utils/routes';
+import * as Action from './action';
+import * as Select from './select';
+
+describe('roles page', () => {
+  beforeEach(() => {
+    cy.seedCollection('roles', 'roles');
+    cy.setUserRoles([SpecialRoleName.MEMBER]);
+    cy.login();
+  });
+
+  after(() => {
+    cy.setPermission(SpecialRoleName.MEMBER, PermissionCategory.ROLE, PermissionPrivilege.NONE);
+  });
+
+  context('with only read permission on roles', () => {
+    beforeEach(() => {
+      cy.setPermission(SpecialRoleName.MEMBER, PermissionCategory.ROLE, PermissionPrivilege.READ);
+      Action.visitRolesPage();
+    });
+
+    it('should display roles and not edition tools', () => {
+      cy.dataCy('admin').should('contain.text', 'Rôles');
+      Select.roleLines().should('have.length', 4);
+      cy.dataCy('admin').should('not.contain.text', 'SuperAdmin');
+      cy.dataCy('create-role').should('not.exist');
+      cy.dataCy('edit').should('not.exist');
+      cy.dataCy('role-0').click();
+      cy.dataCy('edit-role-modal').should('not.exist');
+    });
+  });
+
+  context('with admin permission on roles', () => {
+    beforeEach(() => {
+      cy.setPermission(SpecialRoleName.MEMBER, PermissionCategory.ROLE, PermissionPrivilege.ADMIN);
+      Action.visitRolesPage();
+    });
+
+    it('should display roles and edition tools', () => {
+      cy.dataCy('admin').should('contain.text', 'Rôles');
+      Select.roleLines().should('have.length', 4);
+      cy.dataCy('edit').should('have.length', 4);
+      cy.dataCy('create-role').should('be.visible');
+      cy.dataCy('role-0').click();
+      cy.dataCy('edit-role-modal').should('be.visible');
+    });
+
+    it('should be able to create a role with user read permission', () => {
+      cy.intercept('POST', APIRoute.ROLES).as('createRole');
+
+      cy.dataCy('create-role').click();
+      cy.dataCy('create-role-modal').dataCy('name', 'input').type('New role');
+      Select.reqPermissionsFormSelect().should('be.enabled');
+      cy.dataCy('req-permissions-help').should('not.exist');
+      Select.permissionsFormOption('ROLE', '2_READ_WRITE')
+        .should('be.disabled')
+        .and('contain.text', 'Réservé aux Admins');
+      Select.permissionsFormOption('USER', '2_READ_WRITE')
+        .should('be.disabled')
+        .and('contain.text', 'Doit pouvoir lire les rôles');
+      Select.permissionsFormSelect('USER').select('1_READ').should('have.value', '1_READ');
+      Select.reqPermissionsFormSelect().should('be.enabled');
+      cy.dataCy('req-permissions-help').should('not.exist');
+
+      cy.dataCy('create-role-modal').dataCy('submit', 'button').click();
+      cy.wait('@createRole').should(xhr => {
+        expect(xhr.response?.statusCode).to.eq(201);
+        expect(xhr.request.body).to.eql({
+          name: 'New role',
+          permissions: { USER: '1_READ' },
+          requiredPermissionsToAssign: { USER: '2_READ_WRITE' },
+        });
+      });
+      Select.roleLines().should('have.length', 5).and('contain', 'New role');
+    });
+
+    it('should be able to create a role with user write permission', () => {
+      cy.intercept('POST', APIRoute.ROLES).as('createRole');
+
+      cy.dataCy('create-role').click();
+      cy.dataCy('create-role-modal').dataCy('name', 'input').type('New role');
+      Select.permissionsFormSelect('ROLE').select('1_READ');
+      Select.permissionsFormSelect('USER').select('2_READ_WRITE');
+      Select.reqPermissionsFormSelect().should('be.disabled');
+      cy.dataCy('req-permissions-help').should('be.visible');
+      cy.dataCy('req-permissions-help').trigger('mouseover');
+      cy.get('body').should('contain.text', 'Le rôle Admin est obligatoire');
+      Select.permissionsFormSelect('EVENT').select('2_READ_WRITE');
+
+      cy.dataCy('create-role-modal').dataCy('submit', 'button').click();
+      cy.wait('@createRole').should(xhr => {
+        expect(xhr.response?.statusCode).to.eq(201);
+        expect(xhr.request.body).to.eql({
+          name: 'New role',
+          permissions: { ROLE: '1_READ', USER: '2_READ_WRITE', EVENT: '2_READ_WRITE' },
+          requiredPermissionsToAssign: { USER: '3_ADMIN' },
+        });
+      });
+      Select.roleLines().should('have.length', 5).and('contain', 'New role');
+    });
+
+    it('should be able to edit a role', () => {
+      cy.intercept('PUT', `${APIRoute.ROLES}/*`).as('updateRole');
+
+      cy.dataCy('role-1').dataCy('edit', 'button').click();
+      cy.dataCy('edit-role-modal').dataCy('name', 'input').should('have.value', 'Modo').type('2');
+      Select.permissionsFormSelect('USER').select('1_READ');
+      Select.reqPermissionsFormSelect().select('2_READ_WRITE');
+
+      cy.dataCy('edit-role-modal').dataCy('submit', 'button').click();
+      cy.wait('@updateRole').should(xhr => {
+        expect(xhr.response?.statusCode).to.eq(200);
+        expect(xhr.request.body).to.eql({
+          name: 'Modo2',
+          permissions: { ROLE: '1_READ', USER: '1_READ', EVENT: '2_READ_WRITE' },
+          requiredPermissionsToAssign: { USER: '2_READ_WRITE' },
+        });
+      });
+      Select.roleLines().should('have.length', 4).and('contain', 'Modo2');
+    });
+
+    it('should be able to delete a role', () => {
+      cy.intercept('DELETE', `${APIRoute.ROLES}/*`).as('deleteRole');
+
+      cy.dataCy('role-1').dataCy('edit', 'button').click();
+      cy.dataCy('edit-role-modal').dataCy('delete', 'button').click();
+      cy.dataCy('edit-role-modal').dataCy('confirm-delete', 'button').click();
+      cy.wait('@deleteRole').its('response.statusCode').should('eq', 200);
+      Select.roleLines().should('have.length', 3);
+    });
+
+    it('should not be able to edit admin role', () => {
+      cy.contains(SpecialRoleName.ADMIN).closest('tr').dataCy('edit', 'button').click();
+      cy.dataCy('edit-role-modal').find('input:enabled').should('not.exist');
+      cy.dataCy('edit-role-modal').find('select:enabled').should('not.exist');
+      cy.dataCy('edit-role-modal').dataCy('submit', 'button').should('not.exist');
+      cy.dataCy('edit-role-modal').dataCy('delete', 'button').should('not.exist');
+    });
+
+    it('should be able to partially edit visitor role', () => {
+      cy.intercept('PUT', `${APIRoute.ROLES}/*`).as('updateRole');
+
+      cy.contains(SpecialRoleName.VISITOR).closest('tr').dataCy('edit', 'button').click();
+      cy.dataCy('edit-role-modal').find('input:enabled').should('not.exist');
+      cy.dataCy('edit-role-modal').dataCy('delete', 'button').should('not.exist');
+      cy.dataCy('req-permissions-form').should('not.exist');
+      Select.permissionsFormSelect('USER').select('1_READ');
+
+      cy.dataCy('edit-role-modal').dataCy('submit', 'button').click();
+      cy.wait('@updateRole');
+      cy.dataCy('edit-role-modal').dataCy('close-modal', 'button').click();
+      cy.dataCy('edit-role-modal').should('not.exist');
+      cy.contains(SpecialRoleName.VISITOR)
+        .closest('tr')
+        .contains('Utilisateurs')
+        .closest('tr')
+        .contains('Lecture');
+    });
+
+    it('should be able to partially edit viking role', () => {
+      cy.contains(SpecialRoleName.MEMBER).closest('tr').dataCy('edit', 'button').click();
+      cy.dataCy('edit-role-modal').find('input:enabled').should('not.exist');
+      cy.dataCy('edit-role-modal').dataCy('delete', 'button').should('not.exist');
+      Select.permissionsFormSelect('USER').select('1_READ');
+      Select.reqPermissionsFormSelect().select('3_ADMIN');
+      cy.dataCy('edit-role-modal').dataCy('submit', 'button').should('be.enabled');
+    });
+  });
+});
