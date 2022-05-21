abbf65f642e0d2ee0d2476c2022d6f1bff51b901
Chinimala <astrid.allemandou@free.fr>
2022-05-21 16:49:35 +0200

    test: add chromatic delay to mods and world

--- storybook/pages/Mods.stories.tsx
+++ storybook/pages/Mods.stories.tsx
@@ -3,7 +3,7 @@ import { StoryCategory } from 'storybook/config/constants';
 import ModsPage from 'components/pages/Mods';
 
 const { defaultExport, StoryFactory } = storybookSetup(ModsPage, StoryCategory.PAGE, {
-  parameters: { chromatic: { delay: 1000 } },
+  parameters: { chromatic: { delay: 2000 } },
 });
 
 export default defaultExport;
