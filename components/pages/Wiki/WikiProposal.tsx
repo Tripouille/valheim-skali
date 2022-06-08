24ee8ab009f6ce50a445aa705930e3f451f38259
Chinimala <astrid.allemandou@free.fr>
2022-06-08 20:46:33 +0200

    feat(wiki): temporarily show wiki proposal data from list

--- components/pages/Wiki/WikiProposal.tsx
+++ components/pages/Wiki/WikiProposal.tsx
@@ -1,28 +1,20 @@
-import Background from 'components/core/Containers/Background';
 import { VStack } from 'components/core/Containers/Stack';
-import QueryHandler from 'components/core/Disclosure/QueryHandler';
 import PageTitle from 'components/core/Typography/PageTitle';
 import Text from 'components/core/Typography/Text';
-import { useWikiProposal } from './hooks/useWikiProposal';
+import { WikiProposal } from 'data/wiki';
 
 export interface WikiProposalComponentProps {
-  wikiProposalId: string;
+  wikiProposal: WikiProposal;
 }
 
-const WikiProposalComponent: React.FC<WikiProposalComponentProps> = ({ wikiProposalId }) => {
-  const wikiProposalQuery = useWikiProposal(wikiProposalId);
-  const wikiProposal = wikiProposalQuery.data;
-  const lastSuggestion = wikiProposal?.suggestions.at(-1);
+const WikiProposalComponent: React.FC<WikiProposalComponentProps> = ({ wikiProposal }) => {
+  const lastSuggestion = wikiProposal.suggestions.at(-1);
 
   return (
-    <Background data-cy="events">
-      <QueryHandler query={wikiProposalQuery}>
-        <VStack spacing="7">
-          <PageTitle title={lastSuggestion?.title ?? ''} />
-          <Text>{lastSuggestion?.content}</Text>
-        </VStack>
-      </QueryHandler>
-    </Background>
+    <VStack spacing="7">
+      <PageTitle title={lastSuggestion?.title ?? ''} />
+      <Text>{lastSuggestion?.content}</Text>
+    </VStack>
   );
 };
 
