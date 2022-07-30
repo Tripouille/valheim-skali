62c033f8396e8b3f8266c917fbc555b08f6f7c0d
Chinimala <astrid.allemandou@free.fr>
2022-07-30 22:23:24 +0200

    fix(wiki): remove edit proposal button on wiki page if no permission

--- components/pages/Wiki/WikiPage.tsx
+++ components/pages/Wiki/WikiPage.tsx
@@ -1,6 +1,7 @@
 import NextLink from 'next/link';
 import React from 'react';
 import { GiFeather } from 'react-icons/gi';
+import Secured from 'components/core/Authentication/Secured';
 import Background from 'components/core/Containers/Background';
 import { Grid } from 'components/core/Containers/Grid';
 import { VStack } from 'components/core/Containers/Stack';
@@ -8,6 +9,7 @@ import PageTitle from 'components/core/Typography/PageTitle';
 import { WikiPage } from 'data/wiki';
 import ButtonGroup from 'components/core/Interactive/ButtonGroup';
 import IconButton from 'components/core/Interactive/IconButton';
+import { PermissionCategory, wikiPrivilege } from 'utils/permissions';
 import { NavRoute, serverName } from 'utils/routes';
 import WikiContent from './WikiContent';
 
@@ -22,18 +24,20 @@ const WikiPageComponent: React.FC<WikiPageComponentProps> = ({ wikiPage }) => {
         <Grid w="full" templateColumns="1fr 5fr 1fr" templateAreas='". title buttons"'>
           <PageTitle title={wikiPage.title} gridArea="title" />
           <ButtonGroup gridArea="buttons" justifySelf="end">
-            <NextLink
-              href={`/${serverName}${NavRoute.WIKI}/proposals/new/${wikiPage._id}`}
-              passHref
-            >
-              <IconButton
-                data-cy="edit"
-                aria-label="Proposer une modification"
-                title="Proposer une modification"
-                icon={<GiFeather />}
-                variant="ghost"
-              />
-            </NextLink>
+            <Secured permissions={{ [PermissionCategory.WIKI]: wikiPrivilege.PROPOSE }}>
+              <NextLink
+                href={`/${serverName}${NavRoute.WIKI}/proposals/new/${wikiPage._id}`}
+                passHref
+              >
+                <IconButton
+                  data-cy="edit"
+                  aria-label="Proposer une modification"
+                  title="Proposer une modification"
+                  icon={<GiFeather />}
+                  variant="ghost"
+                />
+              </NextLink>
+            </Secured>
           </ButtonGroup>
         </Grid>
         <WikiContent content={wikiPage.content} />
