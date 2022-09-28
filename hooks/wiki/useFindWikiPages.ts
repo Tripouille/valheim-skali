53ebba86c12a16b0e10000cd110338b1008e7a53
Tripouille <jmichel.gambard@gmail.com>
2022-09-28 11:46:51 +0200

    feat: add google tag manager custom events

--- hooks/wiki/useFindWikiPages.ts
+++ hooks/wiki/useFindWikiPages.ts
@@ -3,8 +3,10 @@ import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import { WikiPage } from 'data/wiki';
 import { APIRoute } from 'utils/routes';
 import { QueryKeys } from 'utils/queryClient';
+import { sendGoogleTagManagerCustomEvent } from 'utils/googleTagManager';
 
 const findWikiPageOnServer = (searchString: string) => async (): Promise<WikiPage[]> => {
+  sendGoogleTagManagerCustomEvent('search_wiki_page', { search_term: searchString });
   const { data } = await axios.post<WikiPage[]>(`${APIRoute.WIKI}/find`, { searchString });
   return data;
 };
