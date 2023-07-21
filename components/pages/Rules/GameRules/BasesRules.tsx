a54979817b4d85ad944188ebf49c55afa506da8a
Chinimala <astrid.allemandou@free.fr>
2023-07-21 21:32:51 +0200

    feat(rules): update rules

--- components/pages/Rules/GameRules/BasesRules.tsx
+++ components/pages/Rules/GameRules/BasesRules.tsx
@@ -37,14 +37,14 @@ const BasesRules = () => {
       />
       <Paragraphs
         paragraphs={[
-          "Et pour finir, vous êtes ici, dans le dixième royaume, pour prouver votre valeur à Odin pas à Loky. Faire le filou ne sera pas bien vu, abuser des mécaniques du jeu non plus. En gros, tout ce qui vous permet d’obtenir des ressources sans effort est interdit. Par exemple, les fermes automatiques (des constructions tuant les monstres automatiquement), ou l’abus des monstres d’un biome pour tuer ceux d’un autre. Il n’est pas interdit d’être astucieux bien sûr, mais il faut juste rester fair-play par rapport aux autres joueurs. Et si vous trouvez un gros raccourci dans le jeu, il n'est probablement pas fair-play.",
+          "Et pour finir, vous êtes ici, dans le dixième royaume, pour prouver votre valeur à Odin pas à Loky. Faire le filou ne sera pas bien vu, abuser des mécaniques du jeu non plus. En gros, tout ce qui vous permet d’obtenir des ressources sans effort est interdit. Par exemple, les fermes automatiques (des constructions tuant les monstres automatiquement), ou l’abus des monstres d’un biome pour tuer ceux d’un autre. Il n’est pas interdit d’être astucieux bien sûr, mais il faut juste rester fair-play par rapport aux autres joueurs. Si vous trouvez un raccourci dans le jeu, qui ne vous fait pas constamment risquer gros, il n'est probablement pas fair-play.",
         ]}
       />
       <br />
       <Paragraphs
         paragraphs={[
           'En tout cas bravo, vous êtes parvenu au bout de l’essentiel du règlement !',
-          'Jetez maintenant un coup d’œil à nos guides de jeu pour des exemples plus concrets, des idées d’activités/quêtes/objectifs à faire chez nous et quelques petites règles supplémentaires. Après quoi vous pourrez (enfin) candidater chez nous. La candidature consiste à remplir un questionnaire sur notre règlement (comme à l’école o/) et ensuite à rejoindre notre discord pour poser vos questions à un de nos membres et enfin nous rejoindre !',
+          'Jetez maintenant un coup d’œil à nos guides de jeu pour des exemples plus concrets, des idées d’activités/quêtes/objectifs à faire chez nous et quelques petites règles supplémentaires. Après quoi vous pourrez (enfin) candidater chez nous. La candidature consiste à remplir un questionnaire sur notre règlement (comme à l’école o/) et ensuite à rejoindre notre discord, donner vos heures de disponibilités pour papoter avec un "sympathiseur" et enfin nous rejoindre !',
           'Pour les streameurs, il y a un règlement de plus à lire (^-^)’',
         ]}
       />
